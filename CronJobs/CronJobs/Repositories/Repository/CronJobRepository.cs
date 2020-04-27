90df45eec66fa14bd54c1c2c535f198f8bc6e3de
xinbo qi <seedqi@126.com>
2020-04-27 19:28:22 +0800

    创建索引

--- CronJobs/CronJobs/Repositories/Repository/CronJobRepository.cs
+++ CronJobs/CronJobs/Repositories/Repository/CronJobRepository.cs
@@ -11,6 +11,23 @@ namespace CronJobs.Repositories.Repository
         public CronJobRepository(MongoClient mongoClient) : base(mongoClient)
         {
             Context = Datebase.GetCollection<CronJob>("CronJobs");
+
+
+            var indexBuilder = Builders<CronJob>.IndexKeys;
+            var keys = indexBuilder.Ascending("Name");
+            var options = new CreateIndexOptions
+            {
+                Name = "idx_name",
+            };
+            var indexModel = new CreateIndexModel<CronJob>(keys, options);
+            Context.Indexes.CreateOneAsync(indexModel).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            var notificationLogBuilder = Builders<CronJob>.IndexKeys;
+            var indexModel1 = new CreateIndexModel<CronJob>(notificationLogBuilder.Ascending(x => x.Name));
+            var a = Context.Indexes.CreateOneAsync(indexModel1).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            //var indexModel = Builders<CronJob>.IndexKeys.Ascending(_ => _.Name);
+            //var a = Context.Indexes.CreateOneAsync(indexModel).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
     }
